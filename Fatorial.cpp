b2b7bcf5c5487aec7f1a67e1334126c85792b8ad
Jean Dálbert <33075545+jdmarcal@users.noreply.github.com>
2018-03-16 23:07:12 -0300

    O programa implementa o cálculo do fatorial de um número informado pelo cliente utilizando recursividade.

---
+++ Fatorial.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//Autor: Jean Dálbert
+//e-mail: dalbertmarcal@gmail.com
+
+//9) O programa abaixo implementa o cálculo do fatorial de um número informado pelo cliente
+//utilizando recursividade.
+
+int FAT (int x);
+
+int main(){
+  
+ int num, fatorial;
+  
+  printf("Digite um numero:");
+  scanf("%d",&num);
+  fatorial = FAT(num);
+ 
+  printf("\n O fatorial de %d e: %d \n",num,fatorial);
+    
+  system("pause");
+  return 0;
+}
+int FAT (int p_num){
+	
+	if(p_num == 0){
+		return 1;
+	}	
+	else{
+	    return p_num * FAT(p_num - 1);	
+	}
+}
+
+//SALVAR ANTES DE COMPILAR!!!!!
